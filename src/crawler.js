abc21605c70ace7077e4f66e69936c7952986541
Honza Charamza <jenda60@gmail.com>
2019-10-21 07:38:43 +0200

    Plné tvary

--- src/crawler.js
+++ src/crawler.js
@@ -4,6 +4,7 @@ const he = require('he');
 const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
+const turf = require('@turf/turf');
 
 var options = {
   attributeNamePrefix: "",
@@ -27,15 +28,15 @@ const TYPE = { WAY: 'way', BUILDING: 'building', AREA: 'area', UNKNOWN: 'unknown
 const TILE_SIZE = zoom => 180.0 / Math.pow(2, zoom);
 
 const getType = way => {
-  if (way.highway || way.waterway) {
-    return { type: TYPE.WAY, subtype: way.highway || way.waterway };
+  if ((way.highway || way.waterway) && !way.area) {
+    return { type: TYPE.WAY, subtype: way.highway || way.waterway, footway: way.footway };
   }
   if (way.building) {
     let type = way.building != 'yes' ? way.building : undefined;
     return { type: TYPE.BUILDING, subtype: type };
   }
-  if (way.area || way.landuse || way.natural) {
-    return { type: TYPE.AREA, subtype: way.natural || way.landuse };
+  if (way.area || way.landuse || way.natural || way.leisure) {
+    return { type: TYPE.AREA, subtype: way.natural || way.landuse || way.leisure || way.highway };
   }
 
   return { type: TYPE.UNKNOWN };
@@ -57,13 +58,10 @@ const getData = (y, x, zoom, isDev = false) => {
 
   const fileName = 'z' + zoom + 'x' + x + 'y' + y + '.json';
   const filePath = path.join(__dirname, '..', '/cache/', fileName);
-  console.log('https://www.openstreetmap.org/api/0.6/map?bbox=' + bbox);
 
-  if (fs.existsSync(filePath)) {
+  if (false && fs.existsSync(filePath)) {
     return JSON.parse(fs.readFileSync(filePath));
   }
-
-  //return fetch('https://www.openstreetmap.org/api/0.6/map?bbox=' + bbox)
 
   return fetch('https://www.openstreetmap.org/api/0.6/map?bbox=' + bbox)
   .then(res => {
@@ -73,56 +71,91 @@ const getData = (y, x, zoom, isDev = false) => {
   .then(xml => parser.getTraversalObj(xml, options))
   .then(obj => {
     const data = parser.convertToJson(obj, options).osm;
-    console.log(data);
     return data;
   })
   .then(json => {
     if (!json.node || !json.way) return {};
     const nodes = {};
+    const ways = {};
     let tmp = {};
 
     Object.keys(TYPE).forEach(key => tmp[TYPE[key]] = []);
 
-    json.node.forEach(node => nodes[node.attr.id] = node);
-    json.way.forEach(way => {
-      const tags = getTagData(way.tag);
-      const type = getType(tags);
-      
-      let obj;
-      if (isDev) {
-        obj = {
-          id: parseInt(way.attr.id, 10),
-          type: type.subtype,
-          shape: []
-        };
-      } else {
-        obj = {
-          type: type.subtype,
-          shape: []
-        };
-      }
+    const closeShape = points => {
+      const first = points[0];
+      const last = points[points.length - 1];
+      if (first.x != last.x || first.y != last.y) points.push(first);
+      return points;
+    }
 
-      const nds = way.nd || [];
-      nds.forEach(nd => {
+    const getShape = nds => {
+      return (nds || []).map(nd => {
         node = nodes[nd.attr.ref];
 
         const x = Math.floor((node.attr.lat - tile.y) / TILE_SIZE(zoom) * 4096);
         const y = Math.floor((node.attr.lon - tile.x) / TILE_SIZE(zoom) * 4096);
 
-        obj.shape.push({x, y});
+        return {x, y};
       });
-
-      tmp[type.type].push(obj);
-    });
-    /*json.relation.forEach(relation => {
-      const tags = getTagData(relation.tag);
+    }
+    const process = (way, nds) => {
+      const id = way.attr.id;
+      const tags = getTagData(way.tag);
       const type = getType(tags);
 
-      const members = relation.member || [];
-      members.forEach(member => {
+      if (type.subtype == 'weir') return;
+      if (tags.crossing) return;
+      
+      let obj = {
+        type: type.subtype,
+        shape: getShape(nds || way.nd)
+      };
 
-      });
-    });*/
+      if (isDev) obj.id = id;
+      if (type.type == "area") obj.shape = closeShape(obj.shape);
+
+      if (obj.type == "forest") {
+        const polygon = turf.polygon([[ ...obj.shape.map(p => [p.x, p.y]) ]]);
+        const clipped = turf.bboxClip(polygon, [0, 0, 4096, 4096]);
+        obj.shape = clipped.geometry.coordinates[0].map(p => ({ x: p[0], y: p[1] }));
+      }
+
+      tmp[type.type].push(obj);
+      ways[id] = way;
+      return obj;
+    };
+
+    if (json.relation && !Array.isArray(json.relation)) json.relation = [json.relation];
+
+    (json.node || []).forEach(node => nodes[node.attr.id] = node);
+    (json.way || []).forEach(way => process(way));
+    (json.relation || []).forEach(relation => {
+      if (!Array.isArray(relation.member)) relation.member = [relation.member];
+      const tags = getTagData(relation.tag);
+
+      if (tags.boundary == "administrative") return;
+
+      let outerWay = null;
+      let innerWays = relation.member.reduce((list, member) => {
+        const ref = parseInt(member.attr.ref);
+
+        if (typeof ways[ref] !== 'undefined') {
+          const way = ways[ref];
+          if (member.attr.role == "outer") outerWay = way;
+          else list.push(way);
+        }
+
+        return list;
+      }, []);
+
+      if (!outerWay) {
+        console.log(JSON.stringify(relation));
+        return;
+      }
+
+      const outer = process(relation, outerWay.nd);
+      outer.inner = innerWays.map(inner => getShape(inner.nd));
+    });
 
     delete tmp.unknown;
 
@@ -138,7 +171,7 @@ const getData = (y, x, zoom, isDev = false) => {
     console.log(error);
     return {
       type: "error",
-      message: error,
+      message: error.message,
     }
   });
 }
